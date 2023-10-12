ffecf77a3ef1d09f423f0968d0399a01987b93e3
jsipkov <jansipkovsky2@gmail.com>
2023-10-12 11:29:28 +0200

    port adj

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import app from './app';
 
-const port = 8080;
+const port = process.env.PORT || 8080;
 
 app.listen(port, () => {
   console.log(`currency-backend application is running on port ${port}.✅`);
