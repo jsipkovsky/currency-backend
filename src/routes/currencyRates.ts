735f02bc6b71c14edb10e91348e4bcd02a23ba86
jsipkov <jansipkovsky2@gmail.com>
2024-07-27 15:05:18 +0200

    get ip

--- src/routes/currencyRates.ts
+++ src/routes/currencyRates.ts
@@ -1,13 +1,14 @@
 import { Request, Response, Router } from 'express';
 import axios from 'axios';
 import { API_URL } from './../config/currencyConfig';
+import { getPublicIP } from '../modules/common';
 
 const router: Router = Router();
 
 router.get('/currency-rates', async (_: Request, res: Response) => {
   try {
-    const response = await axios.get(API_URL);
-    res.send(response.data);
+    const response = await getPublicIP();
+    res.send(response);
   } catch (error) {
     res.status(500).send('Failed to fetch currency rates');
   }
