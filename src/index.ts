a9eba6a8ae13389452a49a83dacf8abf7db3e0d2
jsipkov <jansipkovsky2@gmail.com>
2024-08-08 09:27:44 +0200

    dummy

--- src/index.ts
+++ src/index.ts
@@ -34,7 +34,7 @@ app.listen(port, async () => {
 
     // const bn = await arbitrageManager.withdraw(50, 'binance', 'bingx');
     // try {
-    //   const response = await axios.get('http://ec2-3-70-92-222.eu-central-1.compute.amazonaws.com:3000/api/currencies/binance');
+    //   const response = await axios.get('http://ec2-3-70-92-222.eu-central-1.compute.amazonaws.com:3000/api/withdrwal/binance/USDT/100');
     //   return response;
     // } catch (error) {
     //   console.error('Error fetching public IP:', error);
